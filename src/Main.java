f21f8e1a3182e84dc2897a6ed13677edddaa5d20
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-21 11:45:16 +0530

    calc  commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class Main {
+import java.util.Scanner;
+public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Choose the opertion:");
+        System.out.println("\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.No operation");
+        Scanner xy = new Scanner(System.in);
+        int option = xy.nextInt();
+        System.out.println("Choose the option:");
+        System.out.println("Enter First Number");
+        float First = xy.nextInt();
+        System.out.println("Enter Second Number");
+        float Second = xy.nextInt();
     }
 }
